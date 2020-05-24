b8ca44c5c3ea590d10393b6c64e4026a20b7d342
Conyxxx <43956919+Conyxxx@users.noreply.github.com>
2020-05-24 13:10:48 -0700

    Add files via upload

---
+++ interfaz-v.1/save2.php
@@ -0,0 +1,49 @@
+<?php
+if( isset($_POST['Enviar'])){
+#Los 3 primeros IF muestran por pantalla el formato del archivo el como ha elegido el usuario.
+        if(isset($_POST['IMAGENES'])){
+            echo "FROM ";
+            foreach ($_POST['IMAGENES'] as $image){
+                echo $image . " : ";
+            if(isset($_POST['VERSION'])){
+                foreach($_POST['VERSION'] as $version){
+                    echo $version;
+                }
+            }
+                echo "<br>";
+            }
+
+        }
+
+        if(isset($_POST['ACTUALIZAR'])){
+            echo "RUN apt-get ";
+            foreach ($_POST['ACTUALIZAR'] as $actualizar){
+                echo $actualizar . " ";
+            }
+            echo '&&';
+            echo "<br>";
+        }
+
+        if(isset($_POST['APLICACIONES'])){
+            echo "apt-get install -y ";
+            foreach ($_POST['APLICACIONES'] as $aplicaciones) {
+                echo $aplicaciones . " "  ;
+            }
+            echo "&& ";
+        }
+                #Funcion de guardar en un archivo de texto
+                $doc = fopen("Dockerfile.txt","w+") or die('Cannot open or create the file');
+                fwrite($doc, "FROM " .$image. ":" .$version);
+                fwrite($doc, "\nRUN apt-get " .$actualizar." && \\ \n");
+                fwrite($doc, "apt-get install -y ".$aplicaciones. " && \\ \n");
+                fwrite($doc, "EXPOSE 80 443");
+                fclose($doc);
+                print_r(error_get_last());
+    }
+##Parte de la creacion del contenedor
+$nombre = $_POST['NOMBRE'];
+foreach($nombre as $key => $i){
+    #print "Tu contenedor se ha creado con el nombre de: " .$i."\n";
+    exec("docker build -t" .$i." ." );
+}
+?>
